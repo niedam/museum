e7b88e1989641acfeb8c3064598c677dd230cdbc
Adam <adam.rozenek@gmail.com>
2020-01-14 11:55:17 +0100

    Getting roles, database connection

--- app.py
+++ app.py
@@ -1,26 +1,36 @@
+import psycopg2
 from flask import Flask, request, make_response, render_template, redirect
-from jinja2 import Template, Environment, FileSystemLoader
 
 app = Flask(__name__)
+try:
+    conn = psycopg2.connect("host='labdb' dbname='bd' user='nick' password='haslo'")
+except:
+    print("Karamba");
 
-
-@app.route('/', methods=['GET', 'POST'])
-def hello_world():
-    if (request.method == 'POST'):
-        response = make_response(redirect('/', 303));
-        response.set_cookie('role', request.form['role']);
-        return response
+@app.route('/', methods=['GET'])
+def main_page():
     if (request.cookies.get('role') == "staff"):
-        return "Hello staff <a href='/logout'>logout</a>";
+        return "Hello staff <a href='/logout'>logout</a>"
     elif (request.cookies.get('role') == "visitor"):
-        return "Hello visitor <a href='/logout'>logout</a>";
+        return "Hello visitor <a href='/logout'>logout</a>"
     else:
         return render_template("guest.html", **locals())
 
+
+@app.route('/get/<role>', methods=['GET'])
+def get_role(role):
+    if request.cookies.get('role') == 'staff' or request.cookies.get('role') == 'visitor':
+        return make_response(redirect('/'))
+    if role != 'staff' and role != 'visitor':
+        return make_response(redirect('/'));
+    response = make_response(redirect('/'), 302)
+    response.set_cookie('role', role)
+    return response
+
 @app.route('/logout')
-def hello_world2():
-    resp = make_response(redirect('/'));
-    resp.set_cookie('role', "");
+def logout():
+    resp = make_response(redirect('/'), 302)
+    resp.set_cookie('role', "")
     return resp
 
 
