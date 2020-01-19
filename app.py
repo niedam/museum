6a059eb7f6cf1a6ee1d262fb00d41cc8fedab7a3
Adam <adam.rozenek@gmail.com>
2020-01-19 11:12:44 +0100

    First Jinja template inheritance

--- app.py
+++ app.py
@@ -1,19 +1,21 @@
-import sys
 from functools import wraps
 
 import psycopg2
 from psycopg2 import sql
 
 from dbinfo import *
-from flask import Flask, request, make_response, render_template, redirect
+from flask import Flask, request, make_response, render_template, redirect, send_from_directory
 
 
 app = Flask(__name__)
+
 try:
     conn = psycopg2.connect(host = host, user = user, password = password, dbname = dbname)
     conn.set_client_encoding('UTF8')
 except:
     exit(-1);
+
+
 
 def role_required(f):
     @wraps(f)
@@ -27,27 +29,32 @@ def role_required(f):
 @app.route('/', methods=['GET'])
 @role_required
 def main_page():
+    print("aaaa")
+    return render_template("dashboard.html");
     if (request.cookies.get('role') == "staff"):
         return "Hello staff <a href='/logout'>logout</a>"
     elif (request.cookies.get('role') == "visitor"):
         return "Hello visitor <a href='/logout'>logout</a>"
 
 
-
 @app.route('/get/<role>', methods=['GET'])
 def get_role(role):
+    # User already has had role.
     if request.cookies.get('role') == 'staff' or request.cookies.get('role') == 'visitor':
-        return make_response(redirect('/'))
+        return make_response(redirect('./'))
+    # Wrong role in GET request.
     if role != 'staff' and role != 'visitor':
-        return make_response(redirect('/'));
-    response = make_response(redirect('/'), 302)
+        return make_response(redirect('./'));
+    # Set user role.
+    response = make_response(redirect('./'), 302)
     response.set_cookie('role', role)
     return response
+
 
 @app.route('/logout')
 @role_required
 def logout():
-    resp = make_response(redirect('/'), 302)
+    resp = make_response(redirect('./'), 302)
     resp.set_cookie('role', "")
     return resp
 
@@ -85,6 +92,7 @@ def view(entity, id):
         data = cur.fetchone()
         template = render_template("view_exhibit.html", **locals())
     return template
+
 
 if __name__ == '__main__':
     app.run()
